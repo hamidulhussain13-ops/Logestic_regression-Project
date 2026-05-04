167eddce8885d19c610129713d1dcaef7043858d
Hamidul Hussain <hamidul.hussain13@gmail.com>
2026-05-05 00:01:10 +0530

    Implement Iris dataset training and model saving

---
+++ train.py
@@ -0,0 +1,25 @@
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import joblib
+
+print("Training started...")
+
+# dataset load
+data = load_iris()
+X = data.data
+y = data.target
+
+# split data
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# model
+model = LogisticRegression(max_iter=200)
+model.fit(X_train, y_train)
+
+# save model
+joblib.dump(model, "model.pkl")
+
+print("Model trained and saved successfully!")
